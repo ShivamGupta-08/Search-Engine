adce2a3c69e1fa87eed7cd8cbeaaab7b9ad76b62
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2021-09-18 19:03:55 +0530

    Update Search Engine.py

--- Search Engine.py
+++ Search Engine.py
@@ -7,11 +7,12 @@ def macthingWords (sentence1,sentence2):
             if words1.lower() == words2.lower():
                 score+=1
     return score
-sentences = ["Python is cool", "python is good", "python is not python snake","lol"]
-search = input()
-score = [macthingWords(search,sentence)for sentence in sentences]
-results = [ele for ele in sorted(zip(score,sentences),reverse=True) if  ele[0] != 0]
-print(f"{len(results)} result/s are found!")
-for n,sent in results:
-    print(sent)
+if __name__ == '__main__':       
+    sentences = ["Python is cool", "python is good", "python is not python snake","lol"]
+    search = input()
+    score = [macthingWords(search,sentence)for sentence in sentences]
+    results = [ele for ele in sorted(zip(score,sentences),reverse=True) if  ele[0] != 0]
+    print(f"{len(results)} result/s are found!")
+    for n,sent in results:
+        print(sent)
     
