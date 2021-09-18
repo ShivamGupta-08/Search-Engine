90244cf77c37302636a98f1581b1efee731e5293
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2021-09-18 18:17:22 +0530

    Create Search Engine.py

---
+++ Search Engine.py
@@ -0,0 +1,17 @@
+def macthingWords (sentence1,sentence2):
+    score = 0
+    word1 = sentence1.strip().split(" ")
+    word2 = sentence2.strip().split(" ")
+    for words1 in word1:
+        for words2 in word2:
+            if words1.lower() == words2.lower():
+                score+=1
+    return score
+sentences = ["Python is cool", "python is good", "python is not python snake","lol"]
+search = input()
+score = [macthingWords(search,sentence)for sentence in sentences]
+results = [ele for ele in sorted(zip(score,sentences),reverse=True) if  ele[0] != 0]
+print(f"{len(results)} result/s are found!")
+for n,sent in results:
+    print(sent)
+    
